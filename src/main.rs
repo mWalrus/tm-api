7ebcf7a3d0aad983b06e40f783b0cb6d051d05d1
mWalrus <hellkvistoskar@protonmail.com>
2024-01-23 23:24:52 +0100

    chore: move token refresh thread into token module

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,10 @@ mod records;
 mod routes;
 mod token;
 
-use std::{
-    sync::{Arc, Mutex},
-    thread,
-    time::Duration,
-};
+use std::sync::{Arc, Mutex};
 
-use chrono::{DateTime, NaiveDateTime, Utc};
 use reqwest::Client;
-use token::Token;
+use token::{run_token_thread, Token};
 
 #[launch]
 async fn rocket() -> _ {
@@ -22,40 +17,7 @@ async fn rocket() -> _ {
     };
 
     let token = Arc::new(Mutex::new(Token::auth().await.unwrap()));
-    let token_clone = Arc::clone(&token);
-    thread::spawn(move || loop {
-        let mut rat = None;
-
-        if let Ok(t) = token_clone.lock() {
-            rat = Some(t.rat);
-        }
-
-        if let Some(rat) = rat {
-            let now = Utc::now();
-
-            let time_to_refresh = NaiveDateTime::from_timestamp_millis(rat as i64 * 1000).unwrap();
-            let time_to_refresh = DateTime::from_utc(time_to_refresh, Utc);
-
-            // NOTE: Getting the delta time should not fail unless Nadeo's authentication services
-            // are offline, so we have to keep this failsafe here just in case.
-            let timeout = match (time_to_refresh - now).to_std() {
-                Ok(delta_time) => delta_time,
-                Err(e) => {
-                    eprintln!("ERROR: failed to get token age: {e:?}");
-                    Duration::from_secs(60)
-                }
-            };
-
-            thread::sleep(timeout);
-
-            if let Ok(t) = &mut token_clone.lock() {
-                match t.refresh() {
-                    Ok(_) => println!("INFO: Refreshed access token!"),
-                    Err(e) => eprintln!("ERROR: {e:?}"),
-                }
-            }
-        }
-    });
+    run_token_thread(&token);
 
     rocket::custom(&config)
         .register("/", catchers![routes::not_found, routes::default])
--- src/token.rs
+++ src/token.rs
@@ -1,9 +1,16 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
 use anyhow::anyhow;
 use base64::{
     alphabet,
     engine::{self, general_purpose},
     Engine,
 };
+use chrono::{DateTime, NaiveDateTime, Utc};
 use reqwest::{
     blocking::Client as BlockingClient,
     header::{AUTHORIZATION, CONTENT_TYPE},
@@ -36,8 +43,8 @@ struct TokenPayload {
 pub struct Token {
     pub access_token: String,
     refresh_token: String,
-    pub rat: u64,
-    exp: u64,
+    pub refresh_at: u64,
+    expires_at: u64,
 }
 
 impl<'t> From<(TokenResponse<'t>, TokenPayload)> for Token {
@@ -45,8 +52,8 @@ impl<'t> From<(TokenResponse<'t>, TokenPayload)> for Token {
         Token {
             access_token: res.access_token.into(),
             refresh_token: res.refresh_token.into(),
-            rat: payload.rat,
-            exp: payload.exp,
+            refresh_at: payload.rat,
+            expires_at: payload.exp,
         }
     }
 }
@@ -160,8 +167,8 @@ impl Token {
 
         self.access_token = token_response.access_token.into();
         self.refresh_token = token_response.refresh_token.into();
-        self.rat = token_payload.rat;
-        self.exp = token_payload.exp;
+        self.refresh_at = token_payload.rat;
+        self.expires_at = token_payload.exp;
 
         Ok(())
     }
@@ -169,4 +176,41 @@ impl Token {
     pub fn as_header(&self) -> String {
         format!("nadeo_v1 t={}", self.access_token)
     }
+}
+
+pub fn run_token_thread(token: &Arc<Mutex<Token>>) {
+    let token_clone = Arc::clone(&token);
+    thread::spawn(move || loop {
+        let mut refresh_token_at = None;
+
+        if let Ok(t) = token_clone.lock() {
+            refresh_token_at = Some(t.refresh_at);
+        }
+
+        if let Some(rat) = refresh_token_at {
+            let now = Utc::now();
+
+            let time_to_refresh = NaiveDateTime::from_timestamp_millis(rat as i64 * 1000).unwrap();
+            let time_to_refresh = DateTime::from_utc(time_to_refresh, Utc);
+
+            // NOTE: Getting the delta time should not fail unless Nadeo's authentication services
+            // are offline, so we have to keep this failsafe here just in case.
+            let timeout = match (time_to_refresh - now).to_std() {
+                Ok(delta_time) => delta_time,
+                Err(e) => {
+                    eprintln!("ERROR: failed to get token lifetime: {e:?}");
+                    Duration::from_secs(60)
+                }
+            };
+
+            thread::sleep(timeout);
+
+            if let Ok(t) = &mut token_clone.lock() {
+                match t.refresh() {
+                    Ok(_) => println!("INFO: Refreshed access token!"),
+                    Err(e) => eprintln!("ERROR: {e:?}"),
+                }
+            }
+        }
+    });
 }
